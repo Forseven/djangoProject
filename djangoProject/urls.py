b2e3acfb10af12529f338b571022e84663bc391a
Forseven <nutaru@gmail.com>
2021-05-16 20:42:39 +0700

    lesson 7

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,12 +20,13 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('', mainapp.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact,name='contact'),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
     path('auth/', include('authnapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('admin/', include('adminapp.urls', namespace='admin'))
 
 ]
